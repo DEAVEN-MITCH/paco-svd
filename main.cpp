3aa3d68bad8cb9087e5432480842dda150982900
zhang_jia_he <1654898640@qq.com>
2025-04-16 21:49:15 +0800

    bidiag buffersize decided

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include "utils.h"
 
 #include "acl/acl.h"
-
+#include "aclrtlaunch_upper_bidiagonalization.h"
 #define debug(x) std::cerr << #x << ": " << x << std::endl
 int main(int argc, char **argv)
 {
@@ -38,19 +38,13 @@ int main(int argc, char **argv)
     uint8_t *workspaceDevice;
     CHECK_ACL(aclrtMalloc((void **)&workspaceDevice, workspaceSize, ACL_MEM_MALLOC_HUGE_FIRST));
 
-    uint8_t *aMatrixHost;
-    uint8_t *aMatrixDevice;
-    // CHECK_ACL(aclrtMallocHost((void **)(&aMatrixHost), aMatrixFileSize));
-    // CHECK_ACL(aclrtMalloc((void **)&aMatrixDevice, aMatrixFileSize, ACL_MEM_MALLOC_HUGE_FIRST));
-    // ReadFile("../input/A_gm.bin", aMatrixFileSize, aMatrixHost, aMatrixFileSize);
-    // CHECK_ACL(aclrtMemcpy(aMatrixDevice, aMatrixFileSize, aMatrixHost, aMatrixFileSize, ACL_MEMCPY_HOST_TO_DEVICE));
+    uint8_t *AMatrixHost;
+    uint8_t *AMatrixDevice;
+    CHECK_ACL(aclrtMallocHost((void **)(&AMatrixHost), AMatrixFileSize));
+    CHECK_ACL(aclrtMalloc((void **)&AMatrixDevice, AMatrixFileSize, ACL_MEM_MALLOC_HUGE_FIRST));
+    ReadFile("../input/A_gm.bin", AMatrixFileSize, AMatrixHost, AMatrixFileSize);
+    CHECK_ACL(aclrtMemcpy(AMatrixDevice, AMatrixFileSize, AMatrixHost, AMatrixFileSize, ACL_MEMCPY_HOST_TO_DEVICE));
 
-    // uint8_t *lMatrixHost;
-    // uint8_t *lMatrixDevice;
-    // CHECK_ACL(aclrtMallocHost((void **)(&lMatrixHost), lMatrixFileSize));
-    // CHECK_ACL(aclrtMalloc((void **)&lMatrixDevice, lMatrixFileSize, ACL_MEM_MALLOC_HUGE_FIRST));
-    // ReadFile("../input/L_gm.bin", lMatrixFileSize, lMatrixHost, lMatrixFileSize);
-    // CHECK_ACL(aclrtMemcpy(lMatrixDevice, lMatrixFileSize, lMatrixHost, lMatrixFileSize, ACL_MEMCPY_HOST_TO_DEVICE));
 
     // uint8_t *TilingHost;
     // uint8_t *TilingDevice;
@@ -60,42 +54,42 @@ int main(int argc, char **argv)
     // GenerateTiling(M, N, blockDim, TilingHost, lim);
     // CHECK_ACL(aclrtMemcpy(TilingDevice, TilingFileSize, TilingHost, TilingFileSize, ACL_MEMCPY_HOST_TO_DEVICE));
 
-    // uint8_t *xMatrixHost;
-    // uint8_t *xMatrixDevice;
-    // CHECK_ACL(aclrtMallocHost((void **)(&xMatrixHost), xMatrixFileSize));
-    // CHECK_ACL(aclrtMalloc((void **)&xMatrixDevice, xMatrixFileSize, ACL_MEM_MALLOC_HUGE_FIRST));
+    uint8_t *UMatrixHost;
+    uint8_t *UMatrixDevice;
+    CHECK_ACL(aclrtMallocHost((void **)(&UMatrixHost), UMatrixFileSize));
+    CHECK_ACL(aclrtMalloc((void **)&UMatrixDevice, UMatrixFileSize, ACL_MEM_MALLOC_HUGE_FIRST));
+    CHECK_ACL(aclrtMemset(UMatrixDevice, UMatrixFileSize, 0,UMatrixFileSize));  
 
-    // uint8_t *tMatrixHost;
-    // uint8_t *tMatrixDevice;
-    // CHECK_ACL(aclrtMallocHost((void **)(&tMatrixHost), aMatrixFileSize));
-    // CHECK_ACL(aclrtMalloc((void **)&tMatrixDevice, aMatrixFileSize, ACL_MEM_MALLOC_HUGE_FIRST));
+    uint8_t *VtMatrixHost;
+    uint8_t *VtMatrixDevice;
+    CHECK_ACL(aclrtMallocHost((void **)(&VtMatrixHost), VtMatrixFileSize));
+    CHECK_ACL(aclrtMalloc((void **)&VtMatrixDevice, VtMatrixFileSize, ACL_MEM_MALLOC_HUGE_FIRST));
+    CHECK_ACL(aclrtMemset(VtMatrixDevice, VtMatrixFileSize, 0,VtMatrixFileSize));  
+    ACLRT_LAUNCH_KERNEL(upper_bidiagonalization)
+    (1, stream, AMatrixDevice, UMatrixDevice, VtMatrixDevice, M, N);
+    CHECK_ACL(aclrtSynchronizeStream(stream));
 
-    // ACLRT_LAUNCH_KERNEL(custom_trsm)
-    // (blockDim, stream, aMatrixDevice, lMatrixDevice, xMatrixDevice, tMatrixDevice, workspaceDevice, TilingDevice);
-    // CHECK_ACL(aclrtSynchronizeStream(stream));
+    std::cout << "finish" << std::endl;
 
-    // std::cout << "finish" << std::endl;
+    CHECK_ACL(aclrtMemcpy(UMatrixHost, UMatrixFileSize, UMatrixDevice, UMatrixFileSize, ACL_MEMCPY_DEVICE_TO_HOST));
+    CHECK_ACL(aclrtMemcpy(AMatrixHost, AMatrixFileSize, AMatrixDevice, AMatrixFileSize, ACL_MEMCPY_DEVICE_TO_HOST));
+    CHECK_ACL(aclrtMemcpy(VtMatrixHost, VtMatrixFileSize, VtMatrixDevice, VtMatrixFileSize, ACL_MEMCPY_DEVICE_TO_HOST));
+    WriteFile("../output/U.bin", UMatrixHost, UMatrixFileSize);
+    WriteFile("../output/B.bin", AMatrixHost, AMatrixFileSize);
+    WriteFile("../output/Vt.bin", VtMatrixHost, VtMatrixFileSize);
 
-    // CHECK_ACL(aclrtMemcpy(xMatrixHost, xMatrixFileSize, xMatrixDevice, xMatrixFileSize, ACL_MEMCPY_DEVICE_TO_HOST));
-    // // WriteFile(output_file, xMatrixHost, xMatrixFileSize);
-    // CHECK_ACL(aclrtMemcpy(tMatrixHost, xMatrixFileSize, tMatrixDevice, xMatrixFileSize, ACL_MEMCPY_DEVICE_TO_HOST));
-    // WriteFile(output_file, xMatrixHost, xMatrixFileSize);
+    std::cout << "[MATRIX B]" << std::endl;
+    PrintPartOfMatrix<float>(UMatrixHost, M, N, 8, 8);
 
-    // // CHECK_ACL(aclrtMemcpy(tMatrixHost, xMatrixFileSize, tMatrixDevice, xMatrixFileSize, ACL_MEMCPY_DEVICE_TO_HOST));
-    // std::cout << "[MATRIX X]" << std::endl;
-    // PrintPartOfMatrix<float>(xMatrixHost, M, N, 8, 8);
-
-    // CHECK_ACL(aclrtFree(aMatrixDevice));
-    // CHECK_ACL(aclrtFreeHost(aMatrixHost));
-    // CHECK_ACL(aclrtFree(lMatrixDevice));
-    // CHECK_ACL(aclrtFreeHost(lMatrixHost));
-    // CHECK_ACL(aclrtFree(xMatrixDevice));
-    // CHECK_ACL(aclrtFreeHost(xMatrixHost));
-    // CHECK_ACL(aclrtFree(tMatrixDevice));
-    // CHECK_ACL(aclrtFreeHost(tMatrixHost));
+    CHECK_ACL(aclrtFree(AMatrixDevice));
+    CHECK_ACL(aclrtFreeHost(AMatrixHost));
+    CHECK_ACL(aclrtFree(UMatrixDevice));
+    CHECK_ACL(aclrtFreeHost(UMatrixHost));
+    CHECK_ACL(aclrtFree(VtMatrixDevice));
+    CHECK_ACL(aclrtFreeHost(VtMatrixHost));
     // CHECK_ACL(aclrtFree(TilingDevice));
     // CHECK_ACL(aclrtFreeHost(TilingHost));
-    // CHECK_ACL(aclrtFree(workspaceDevice));
+    CHECK_ACL(aclrtFree(workspaceDevice));
 
     CHECK_ACL(aclrtDestroyStream(stream));
     CHECK_ACL(aclrtResetDevice(deviceId));
